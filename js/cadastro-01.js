072aebed90ec9097c40f5df6c6382e966a610bc2
arthur972645 <arthurall.1973@gmail.com>
2023-12-06 19:32:13 -0300

    ralizando js dos cadastros e login

---
+++ js/cadastro-01.js
@@ -0,0 +1,55 @@
+// const form1 = document.getElementById("form1");
+// const form2 = document.getElementById("form2");
+// const form3 = document.getElementById("form3");
+// const imputnome = document.getElementById("imput-nome");
+// const imputsobrenome = document.getElementById("imput-sobrenome");
+// const imputcpf = document.getElementById("imput-cpf");
+// const imputnascimento = document.getElementById("imput-nascimento");
+// const imputgenero = document.getElementById("imput-genero");
+// const imputemail = document.getElementById("imput-email");
+// const imputsenha = document.getElementById("imput-senha");
+
+
+// function checkInputNome(){
+//     const imputnomeValue = imputnome.value;
+//     console.log(imputnomeValue)
+// } 
+
+// function checkInputNome(){
+//     const imputnomeValue = imputnome.value;
+    
+//     if(imputnome === ""){
+//        console.log(erro);
+//     }else{
+//         const form1Item = imputnome.parentElement;
+//         form1Item.className = "form-content"
+//       }
+// }
+
+
+// function checkForm1(){
+//     imputnome();
+    
+  
+//     const form1Items = form1.querySelectorAll(".form-content")
+  
+//     const isValid = [...form1Items].every( (item) => {
+//       return item.className === "form-content"
+//     });
+  
+//     if(isValid){
+//       alert("CADASTRADO COM SUCESSO!")
+//     }
+  
+//   }
+  
+  
+//   function errorInput(input, message){
+//     const form1Item = input.parentElement;
+//     const textMessage = form1Item.querySelector("a")
+  
+//     textMessage.innerText = message;
+  
+//     form1Item.className = "form-content error"
+  
+//   }
